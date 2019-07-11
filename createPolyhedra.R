23df417509b020483ba29139b627e8ec6482014d
Otto from Mac <operdeck@gmail.com>
2019-07-11 12:30:23 +0200

    Can create duals and now all platonic and some of the regular stellars

--- createPolyhedra.R
+++ createPolyhedra.R
@@ -1,6 +1,6 @@
 library(rgl)
 
-open3d()
+#open3d()
 #shade3d( icosahedron3d() )
 #writeOBJ(filename)
 
@@ -19,11 +19,11 @@ deltaEquals <- function(x, y, delta = 1e-6)
 
 drawPoly <- function(p)
 {
-  rgl.clear()
   rgl.texts(p$vertices$x, p$vertices$y, p$vertices$z, text = seq(nrow(p$vertices)), color="blue")
   if (length(p$faces) > 0) {
     for (f in seq(length(p$faces))) {
       # triangulize: draw triangles between center of face and all edges
+      # TODO consider skipping this if already a triangle
       cx = mean(p$vertices$x[p$faces[[f]]])
       cy = mean(p$vertices$y[p$faces[[f]]])
       cz = mean(p$vertices$z[p$faces[[f]]])
@@ -31,7 +31,8 @@ drawPoly <- function(p)
         p1 <- p$faces[[f]][t]
         p2 <- p$faces[[f]][(t %% length(p$faces[[f]])) + 1]
         # NB not sure about the orientation of the triangle - may have to check on this
-        triangles3d( c(p$vertices$x[c(p1,p2)],cx), c(p$vertices$y[c(p1,p2)],cy), c(p$vertices$z[c(p1,p2)],cz), col="red")
+        triangles3d( c(p$vertices$x[c(p1,p2)],cx), c(p$vertices$y[c(p1,p2)],cy), c(p$vertices$z[c(p1,p2)],cz), 
+                     col=rainbow(length(p$faces))[f]) # "red"
       }
     }
   }
@@ -40,135 +41,218 @@ drawPoly <- function(p)
 
 # given a distance d and a schaffly (?) description eg {4, 3}
 
-# to build a new regular face with E edges
-# 1. choose a first vertex that is not fully taken yet (less than F faces containing this)
-# 2. choose a second vertex that has distance d to the first one (and has not been taken)
-# 3. choose a third vertex that has distance d, has not been taken, and builds a face {1,2,3} with a normal pointing outwards
-# (if we need more)
-# 4. for any next vertex, choose so that distance fullfilled, hasnt been taken, and is in the same plane as 1-2-3
 
-# start with initial just for testing
-# poly$faces[[1]] <- c(1, 2, 4, 3)
-# poly$faces[[2]] <- c(1, 3, 7, 5)
-# poly$faces[[3]] <- c(3, 4, 8, 7)
 
-# the last one also needs to have desired distance to the first vertex so at this point
-# v1 should be in the 2nd half of above expression, if not --> fail
 
-# any further points need to be in the same plane
-# plane can be formed after 3 pts https://mathinsight.org/forming_planes
-# dist to plane: https://mathinsight.org/distance_point_plane
 
-buildFace <- function(p, aFace = c())
+# Build up a face from existing vertices in given polygon, within constraints passed in as
+# the number of edges/vertices of this face, number of faces per vertex and the length of
+# an edge. The face does not need to be regular but will have all edges of the same length.
+buildFace <- function(p, polygonsize, vertexsize, edgelength, aFace = c(), debug = F)
 {
-  print(aFace)
-
+  if (debug) print(aFace)
+  
   # If we're done, verify that the face is not already in the polygon
-  if (length(aFace) == p$polygonsize) {
+  if (length(aFace) == polygonsize) {
     if (length(p$faces) > 0) {
       c1 <- sort(aFace)
       for (f in seq(length(p$faces))) {
         if (identical(c1, sort(p$faces[[f]]))) {
-          cat("Duplicate", fill=T)
+          if (debug) cat("Duplicate", fill=T)
           return(NULL)
         }
       }
     }
-    cat("We're OK", fill=T)
+    if (debug) cat("Face complete", fill=T)
     # consider turning it upside down here to normalize the normal
     return(aFace)
   }
   
   # Check vertex not fully occupied
-  candidates <- setdiff(which( sapply(seq(nrow(p$vertices)), function(i) { sum(unlist(p$faces) == i)}) < p$facesperedge ), aFace)
-  cat("Free vertices:", candidates, fill = T)
+  candidates <- setdiff(which( sapply(seq(nrow(p$vertices)), function(i) { sum(unlist(p$faces) == i)}) < vertexsize ), aFace)
+  if (debug) cat("Free vertices:", candidates, fill = T)
   
   if (length(candidates) == 0) { return(FALSE) }
   
   # Check vertex right distance to previous point
   if (length(aFace) > 0) {
-    candidates <- candidates[which(deltaEquals(distance(p$vertices[aFace[length(aFace)],], p$vertices[candidates,]), p$edgesize))]  
-    cat("Right distance to prev:", candidates, fill = T)
+    candidates <- candidates[which(deltaEquals(distance(p$vertices[aFace[length(aFace)],], p$vertices[candidates,]), edgelength))]  
+    if (debug) cat("Right distance to prev:", candidates, fill = T)
   }
   
   # Check last vertex right distance to first point
-  if (length(aFace) == (p$polygonsize-1)) {
-    candidates <- candidates[which(deltaEquals(distance(p$vertices[aFace[1],], p$vertices[candidates,]), p$edgesize))]  
-    cat("Right distance to first:", candidates, fill = T)
+  if (length(aFace) == (polygonsize-1)) {
+    candidates <- candidates[which(deltaEquals(distance(p$vertices[aFace[1],], p$vertices[candidates,]), edgelength))]  
+    if (debug) cat("Right distance to first:", candidates, fill = T)
   }
-    
-  # for polysize > 3 check they're all in the same plane
   
+  # TODO:  
+  # for polysize > 3 check they're all in the same plane
+  # any further points need to be in the same plane
+  # plane can be formed after 3 pts https://mathinsight.org/forming_planes
+  # dist to plane: https://mathinsight.org/distance_point_plane
+  
+  # Loop over the remaining candidates and try them out
   for (c in candidates) {
-    f <- buildFace(p, c(aFace, c))
+    f <- buildFace(p, polygonsize, vertexsize, edgelength, c(aFace, c), debug)
     if (!is.null(f)) { return(f) }
   }
   
   return(NULL)
 }
 
-buildPoly <- function(poly)
+# Build a polygon given a set of vertices (full x, y, z coordinates), the two vertices
+# of an example edge (used to determine the global edge size of this polyhedron)
+buildRegularPoly <- function(vertices, polygonsize, vertexsize, exampleEdge = c(1,2), debug=F)
 {
+  # Scale the vertices to unit length
+  vertices <- vertices / apply(vertices, 1, function(r) { sqrt(sum(r^2)) })
+  
+  # Determine global edge size from given example edge
+  edgelength <- distance(vertices[exampleEdge[1],], vertices[exampleEdge[2],])
+  
+  # Add new faces one by one until all vertices have the specified number of faces
+  poly <- list(vertices = vertices, faces = list())
   repeat {
     # Check if there's any not fully occupied vertices
-    freeVertices <- which( sapply(seq(nrow(poly$vertices)), function(i) { sum(unlist(poly$faces) == i)}) < poly$facesperedge )
+    freeVertices <- which( sapply(seq(nrow(poly$vertices)), function(i) { sum(unlist(poly$faces) == i)}) < vertexsize )
     if (length(freeVertices) == 0) {
-      print("All done!")
-      cat("The polyhedron has", length(poly$faces), "faces,", length(poly$faces)*poly$polygonsize/2, "edges,", nrow(poly$vertices), "vertices", fill = T)
-      return(poly)
+      if (debug) print("All done!")
+      break
     }
-  
-    cat("Building face:", length(poly$faces) + 1, fill=T)
     
-    f <- buildFace(poly)
-    print(f)
+    if (debug) cat("Building face:", length(poly$faces) + 1, fill=T)
+    
+    f <- buildFace(poly, polygonsize, vertexsize, edgelength, debug = debug)
+    if (debug) print(f)
     
     if (!is.null(f)) {
       poly$faces[[length(poly$faces) + 1]] <- f
-      drawPoly(poly)
+      if (debug) drawPoly(poly)
     } else {
-      print("Can't construct polygon!")
+      if (debug) print("Can't construct polygon!")
+      poly$faces <- list()
       break
     }
   } 
+  
+  poly[["n_faces"]] <- length(poly$faces)
+  poly[["n_edges"]] <- length(poly$faces) + nrow(poly$vertices) - 2 # Euler's formula
+  poly[["n_vertices"]] <- nrow(poly$vertices)
+  
+  return(poly)
 }
 
-tetrahedron <- buildPoly(list(vertices = rbind(data.frame(x=1, y=1, z=1), data.frame(x=1, y=-1, z=-1), data.frame(x=-1, y=1, z=-1), data.frame(x=-1, y=-1, z=1)),
-                              faces = list(),
-                              edgesize = sqrt(8),
-                              facesperedge = 3,
-                              polygonsize = 3))
+
+# Create dual polygon from given. Vertices become faces and vice versa.
+dual <- function(p)
+{
+  # for every face, calculate the midpoint
+  newVertices <- as.data.frame(t(sapply(seq(length(p$faces)), function(f) { apply(p$vertices[p$faces[[f]],], 2, mean) })))
+  # rescale to unit lenth (NB may not apply to all inverses - TODO reconsider)
+  newVertices <- newVertices / apply(newVertices, 1, function(r) { sqrt(sum(r^2)) })
+  # vertices become the new faces
+  newFaces <- list()
+  for (v in seq(nrow(p$vertices))) {
+    # which faces contain vertex v  
+    connectedFaces <- which(sapply(p$faces, function(f) { return (v %in% f) }))
+    # but they can be in an arbitrary order
+    f1 <- connectedFaces[1]
+    
+    aNewFace <- c()
+    neighbourVertices <- c()
+    for (i in seq(length(connectedFaces))) {
+      # an edge from v along f1 to next point w (so the edge v-w is an edge of f1) - there should be two depending on direction chosen,
+      # then choose the one not already used
+      w_up <- p$faces[[f1]] [(which(p$faces[[f1]] == v) %% 3) + 1]
+      w_down <- p$faces[[f1]] [((which(p$faces[[f1]] == v) + 1 + length(p$faces[[f1]])) %% 3) + 1]
+      w <- ifelse(w_up %in% neighbourVertices, w_down, w_up)
+      # next face is the other face (there can be only 1) that contains v-w or w-v
+      f2 <- setdiff(which(sapply(p$faces, function(f) { return ((w %in% f) & (v %in% f)) })), f1)
+      aNewFace <- c(aNewFace, f1)
+      neighbourVertices <- c(neighbourVertices, w)
+      f1 <- f2
+    }
+    newFaces[[v]] <- aNewFace
+  }
+  poly <- list(vertices = newVertices, faces = newFaces)
+  
+  poly[["n_faces"]] <- length(poly$faces)
+  poly[["n_edges"]] <- length(poly$faces) + nrow(poly$vertices) - 2 # Euler's formula
+  poly[["n_vertices"]] <- nrow(poly$vertices)
+  
+  return(poly)
+}
+
+open3d()
+rgl.clear()
+
+tetrahedron <- buildRegularPoly(vertices = rbind(data.frame(x=1, y=1, z=1), data.frame(x=1, y=-1, z=-1), data.frame(x=-1, y=1, z=-1), data.frame(x=-1, y=-1, z=1)),
+                                polygonsize = 3,
+                                vertexsize = 3,
+                                debug = T)
+drawPoly(tetrahedron)
+
+tetrahedron2 <- dual(tetrahedron)
+drawPoly(tetrahedron2)
+
+# TODO : compose and identification different bodies
+#tetrahedronWithDual <- compose(tetrahedron, tetrahedron2)
+# identify bodies - list is usually just list of faces - helps colouring
 
 # cube.vertices <- expand.grid(x = c(-1, 1), y = c(-1, 1), z = c(-1, 1))
 # cube.d <- distance(cube.vertices[1,], cube.vertices[2, ])
-# cube.facesperedge <- 3
+# cube.vertexsize <- 3
 # cube.polygonsize <- 4
 
-# octahedron.vertices <- rbind(expand.grid(x = c(-1,1), y = 0, z = 0), expand.grid(x = 0, y = c(-1,1), z = 0), expand.grid(x = 0, y = 0, z = c(-1,1)))
-# octahedron.d <- distance(octahedron.vertices[1,], octahedron.vertices[3, ])
-# octahedron.facesperedge <- 4
-# octahedron.polygonsize <- 3
+octahedron <- buildRegularPoly(vertices = rbind(expand.grid(x = c(-1,1), y = 0, z = 0), expand.grid(x = 0, y = c(-1,1), z = 0), expand.grid(x = 0, y = 0, z = c(-1,1))),
+                               polygonsize = 3,
+                               vertexsize = 4,
+                               exampleEdge = c(1,3))
+drawPoly(octahedron)
 
-octahedron <- buildPoly(list(vertices = rbind(expand.grid(x = c(-1,1), y = 0, z = 0), expand.grid(x = 0, y = c(-1,1), z = 0), expand.grid(x = 0, y = 0, z = c(-1,1))),
-                              faces = list(),
-                              edgesize = sqrt(2),
-                              facesperedge = 4,
-                              polygonsize = 3))
+cube <- dual(octahedron)
+drawPoly(cube)
 
-# poly <- list(vertices = octahedron.vertices,
-#              faces = list(),
-#              edgesize = sqrt(2),
-#              facesperedge = 4,
-#              polygonsize = 3)
-# 
-# poly <- list(vertices = tetrahedron.vertices,
-#              faces = list(),
-#              edgesize = sqrt(8),
-#              facesperedge = tetrahedron.facesperedge,
-#              polygonsize = tetrahedron.polygonsize)
+# all coords taken from https://en.wikipedia.org/wiki/Platonic_solid
+phi <- (1+sqrt(5))/2
 
-#drawPoly(poly)
+# dodecahedron seems to work even lacking same-plane condition
+# dodecahedron <- buildRegularPoly(vertices = rbind(expand.grid(x = c(-1,1), y = c(-1,1), z = c(-1,1)), 
+#                                                   expand.grid(x = 0, y = c(-1/phi,1/phi), z = c(-phi, phi)), 
+#                                                   expand.grid(x = c(-1/phi,1/phi), y = c(-phi, phi), z = 0),
+#                                                   expand.grid(x = c(-phi, phi), y = 0, z = c(-1/phi,1/phi))),
+#                                  polygonsize = 5,
+#                                  vertexsize = 3,
+#                                  exampleEdge = c(1,9),
+#                                  debug = T)
 
+icosahedron <- buildRegularPoly(vertices = rbind(expand.grid(x = 0, y = c(-1,1), z = c(-phi, phi)), 
+                                                 expand.grid(x = c(-1,1), y = c(-phi, phi), z = 0), 
+                                                 expand.grid(x = c(-phi, phi), y = 0, z = c(-1, 1))),
+                                polygonsize = 3,
+                                vertexsize = 5,
+                                debug = F)
+
+drawPoly(icosahedron)
+
+dodecahedron <- dual(icosahedron)
+drawPoly(dodecahedron)
+
+greatIcosahedron <- buildRegularPoly(icosahedron$vertices,
+                                     polygonsize = 3,
+                                     vertexsize = 5,
+                                     exampleEdge = c(2, 6))
+drawPoly(greatIcosahedron)
+
+greatStellatedDodecahedron <- dual(greatIcosahedron)
+drawPoly(greatStellatedDodecahedron)
+
+# NB below one not working yet
+compound5tetrahedra <- buildRegularPoly(dodecahedron$vertices,
+                                        polygonsize = 3,
+                                        vertexsize = 3,
+                                        exampleEdge = c(8, 15), debug = T)
 # Build up our base polyhedra
 
 
@@ -180,5 +264,12 @@ octahedron <- buildPoly(list(vertices = rbind(expand.grid(x = c(-1,1), y = 0, z 
 # cube <- inversePoly(octahedron)
 
 # then compose etc will be interesting
+
+# rgl.close()
+
+# colouring
+# if there multiple bodies -> each its own color
+# else if there are multiple types of faces -> each its own color
+# else rainbow
 
 
