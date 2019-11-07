2abfab30d22b94d3d652faedd7cdc852d8881a26
Perdeck, Otto <Otto.Perdeck@pega.com>
2019-11-07 15:38:20 +0100

    Works for single shot

--- layout.R
+++ layout.R
@@ -8,6 +8,7 @@ dodecahedron <- dual(icosahedron, name = "Dodecahedron")
 
 xx <- quasi(cube)
 xx <- rhombic(dodecahedron)
+xx <- tetrahedron
 clear3d()
 drawPoly(xx, debug=T)
 
@@ -49,162 +50,122 @@ projectFace <- function(poly, faceno)
                                  nrow = nrow(coords3D))))
 }
 
+# put a new face into position, given a global 2D layout
+positionNextFace <- function(polyhedron3D, edge, placedFaces) 
+{
+  if (polyhedron3D$edgeToFaces[edge,1] %in% placedFaces) {
+    placedFaceIdx <- polyhedron3D$edgeToFaces[edge,1]
+    newFaceIdx <- polyhedron3D$edgeToFaces[edge,2]
+  } else {
+    newFaceIdx <- polyhedron3D$edgeToFaces[edge,1]
+    placedFaceIdx <- polyhedron3D$edgeToFaces[edge,2]
+  }
+  vertices <- ((which(polyhedron3D$coordPairToEdge==edge)-1) %/% nrow(polyhedron3D$coordPairToEdge))+1
+  cat("Placed:", placedFaceIdx, ", adding:", newFaceIdx, fill = T)
+  cat("vertices:", paste(vertices, collapse=","), fill=T)
+  
+  # place the to be added face on 2D plane
+  face2D <- projectFace(polyhedron3D, newFaceIdx)
+  drawFace2D(face2D, color="green")
+  
+  # lookup the one that is placed already
+  currentFace2D <- layout[[which(sapply(layout, function(x){return(x$face)})==placedFaceIdx)]]
+  
+  # identify the vertices of the edge in both faces
+  if (length(vertices) != 2) stop("An edge must have 2 vertices")
+  p1current <- which(currentFace2D$points3D == vertices[1])
+  p1other <- which(face2D$points3D == vertices[1])
+  p2current <- which(currentFace2D$points3D == vertices[2])
+  p2other <- which(face2D$points3D == vertices[2])
+  if (length(c(p1current,p1other,p2current,p2other)) != 4) stop("All vertices must occur in both faces")
+  
+  # rotate and translate the connected face
+  vecCurrent <- as.numeric(currentFace2D$coords2D[p2current,]) - as.numeric(currentFace2D$coords2D[p1current,])
+  vecOther <- as.numeric(face2D$coords2D[p2other,]) - as.numeric(face2D$coords2D[p1other,])
+  theta <-  vectorAngle2D(vecCurrent, vecOther)
+  rotation <- rotationMatrix2D(theta)
+  
+  # transform points P of other face
+  # as: p1current + rotation (P - p1other)
+  moved <- sweep(face2D$coords2D, 2, as.numeric(face2D$coords2D[p1other,]))
+  rotated <- t(rotation %*% t(moved))
+  translated <- sweep(rotated, 2, as.numeric(currentFace2D$coords2D[p1current,]), "+")
+  face2D$coords2D <- translated
+  
+  return(face2D)
+}
+
+bestResult <- -Inf
+stop()
+
+##### todo refactor until clean then make recursive
 
 # for layout use a new device
 rgl_init(new.device = T)
 clear3d()
 drawAxes()
 
-#drawFace2D(layout[[currentFace]])
-
-bestResult <- -Inf
-
-findBestLayout <- function(p, j)
-{
-  # Faces in the layout 1..j
-  placedFaces <- sapply(layout[1:j], function(f) {return(f$face)})
-  
-  # Candidate edges have one face in the placed faces and one that is not
-  candidateEdges <- which((p$edgeToFaces[,1] %in% placedFaces & !(p$edgeToFaces[,2] %in% placedFaces)) |
-                            (p$edgeToFaces[,2] %in% placedFaces & !(p$edgeToFaces[,1] %in% placedFaces)))
-  
-  # No candidates? We may be done!
-  if (length(candidateEdges) == 0) {
-    cat("No candidate edges at level", j, fill=T)
-    return
-  }
-  
-  cat("Level", j, "candidate edges:", candidateEdges, fill=T)
-  
-  # Try all of them
-  for (e in candidateEdges) {
-    cat("Level", j, "trying edge", e, fill=T)
-    
-    # Determine faces and vertices around this edge
-    if (p$edgeToFaces[e,1] %in% placedFaces) {
-      currentFace <- p$edgeToFaces[e,1]
-      otherFace <- p$edgeToFaces[e,2]
-    } else {
-      otherFace <- p$edgeToFaces[e,1]
-      currentFace <- p$edgeToFaces[e,2]
-    }
-    vertices <- ((which(p$coordPairToEdge==e)-1) %/% nrow(p$coordPairToEdge))+1
-    if (length(vertices) != 2) stop("An edge must have 2 vertices")
-    cat("Level", j, "placed:", currentFace, ", new face:", otherFace, fill = T)
-    cat("Level", j, "vertices:", vertices, fill=T)
-    
-    # Project the new face to 2D
-    newFace2D <- projectFace(p, otherFace) 
-    drawFace2D(newFace2D, color="blue")
-    
-    ## TODO below
-    ## layout[[j]] not good
-    ## instead find which in layout[[1..j]] matches "currentFace" and use that index
-    ## however with just one j doesnt matter
-  
-    # identify where the edge is in both faces
-    p1current <- which(layout[[j]]$points3D == vertices[1])
-    p1other <- which(layout[[j]]$points3D == vertices[1])
-    p2current <- which(newFace2D$points3D == vertices[2])
-    p2other <- which(newFace2D$points3D == vertices[2])
-    if (length(c(p1current,p1other,p2current,p2other)) != 4) stop("All vertices must occur in both faces")
-    
-    # determine the rotation of the new face to put it next to the edge
-    vecCurrent <- as.numeric(layout[[j]]$coords2D[p2current,]) - as.numeric(layout[[j]]$coords2D[p1current,])
-    vecOther <- as.numeric(newFace2D$coords2D[p2other,]) - as.numeric(newFace2D$coords2D[p1other,])
-    theta <-  vectorAngle2D(vecCurrent, vecOther)
-    rotation <- rotationMatrix2D(theta)
-    
-    # do the actual rotation and translation
-    # as: p1current + rotation (P - p1other)
-    moved <- sweep(newFace2D$coords2D, 2, as.numeric(newFace2D$coords2D[p1other,]))
-    rotated <- t(rotation %*% t(moved))
-    translated <- sweep(rotated, 2, as.numeric(layout[[j]]$coords2D[p1current,]), "+")
-    newFace2D$coords2D <- translated
-    drawFace2D(newFace2D, color="green") # NOT GOOD, places inside by accident...
-    
-    # Now, evaluate the new layout with newFace2D added
-    # if already worse then go to next
-    
-    # If we have now placed all faces then evaluate
-    # and store the metric as best, keeping this layout somewhere
-    # and go to next
-    
-    # extend layout with newFace2D
-    # and call findBestLayout with j+1
-  }
-}
-
 # Start with a layout with just the first face
 layout <- list()
 
 # Place first face on 2D plane - does not matter where we start
-face2D <- projectFace(xx, 4) # just to put off the algo - set to 1 later again
-layout[[1+length(layout)]] <- face2D
-
-findBestLayout(xx, 1)
+face2D <- projectFace(xx, 1) # just to put off the algo - set to 1 later again
 
 
 # repeat from here
 repeat {
-  # find a free edge
-  # one of xx$edgeToFaces that has [,1] in layout and [,2] not (or vice versa)
-  placedFaces <- sapply(layout, function(f) {return(f$face)})
-  candidateEdges <- which((xx$edgeToFaces[,1] %in% placedFaces & !(xx$edgeToFaces[,2] %in% placedFaces)) |
-                            (xx$edgeToFaces[,2] %in% placedFaces & !(xx$edgeToFaces[,1] %in% placedFaces)))
-  if (length(candidateEdges) == 0) {
+  
+  # place face2D at this level which will become another function argument
+  layout[[1+length(layout)]] <- face2D
+  drawFace2D(face2D, color="red")
+  level <- length(layout) # prep for search version, this will become a function argument (?)
+  
+  cat("Level", level, "placed face:", face2D$face, fill = T)
+  
+  # update the list of available edges, and store it with the layout itself
+  shiftFacePoints <- shiftrotate(face2D$points3D)
+  newEdges <- sapply(seq(length(face2D$points3D)), function(i) {return(xx$coordPairToEdge[face2D$points3D[i],shiftFacePoints[i]])})
+  if (level > 1) {
+    layout[[level]]$candidateEdges <-
+      setdiff(c(newEdges, layout[[level-1]]$candidateEdges), intersect(layout[[level-1]]$candidateEdges, newEdges))
+  } else {
+    layout[[level]]$candidateEdges <- newEdges
+  }
+  
+  # keep track of the layout min/max
+  newMinCoords <- apply(face2D$coords2D,2,min)
+  newMaxCoords <- apply(face2D$coords2D,2,max)
+  if (level > 1) {
+    layout[[level]]$minCoords <- apply(matrix(c(newMinCoords, layout[[level-1]]$minCoords), ncol=2, byrow = T), 2, min)
+    layout[[level]]$maxCoords <- apply(matrix(c(newMaxCoords, layout[[level-1]]$maxCoords), ncol=2, byrow = T), 2, max)
+  } else {
+    layout[[level]]$minCoords <- newMinCoords
+    layout[[level]]$maxCoords <- newMaxCoords
+  }
+  lines3d( x = c(layout[[level]]$minCoords[1], layout[[level]]$maxCoords[1], layout[[level]]$maxCoords[1], layout[[level]]$minCoords[1], layout[[level]]$minCoords[1]),
+           y = c(layout[[level]]$minCoords[2], layout[[level]]$minCoords[2], layout[[level]]$maxCoords[2], layout[[level]]$maxCoords[2], layout[[level]]$minCoords[2]),
+           z = 0,
+           color="yellow")
+  
+  # get list of all faces placed
+  placedFaces <- sapply(layout[1:level], function(f) {return(f$face)})
+  
+  # check if we're done
+  if (length(layout[[level]]$candidateEdges) == 0) {
     # Done! (or perhaps need to place unconnected face)
+    # Evaluate it here
     break
   }
   
+  # this will become a loop over all candidates
+  edge <- rev(layout[[level]]$candidateEdges)[1] # TODO iterate/recurse here and fail if there are none
   
+  # place it into position, given the current layout (this involves a lot of linear algebra)
+  face2D <- positionNextFace(xx, edge, placedFaces) 
   
-  candidateEdge <- rev(candidateEdges)[1] # TODO iterate/recurse here and fail if there are none
-  if (xx$edgeToFaces[candidateEdge,1] %in% placedFaces) {
-    currentFace <- xx$edgeToFaces[candidateEdge,1]
-    otherFace <- xx$edgeToFaces[candidateEdge,2]
-  } else {
-    otherFace <- xx$edgeToFaces[candidateEdge,1]
-    currentFace <- xx$edgeToFaces[candidateEdge,2]
-  }
-  vertices <- ((which(xx$coordPairToEdge==candidateEdge)-1) %/% nrow(xx$coordPairToEdge))+1
-  cat("Placed:", currentFace, ", adding:", otherFace, fill = T)
-  cat("vertices:", paste(vertices, collapse=","), fill=T)
-  
-  # place it on 2D plane
-  coords3D <- xx$coords[xx$faces[[otherFace]],]
-  angles <- innerAngles(coords3D)
-  radii <- distance(coords3D)
-  offset <- c(5,2)
-  layout[[otherFace]] <- list( face = otherFace, 
-                               points3D = xx$faces[[otherFace]],
-                               coords2D = matrix(c(offset[1] + cos(cumsum(angles)) * radii, y = offset[2] + sin(cumsum(angles)) * radii),nrow = nrow(coords3D)))
-  
-  drawFace2D(layout[[otherFace]])
-  
-  # identify the vertices of the edge in both faces
-  if (length(vertices) != 2) stop("An edge must have 2 vertices")
-  p1current <- which(layout[[currentFace]]$points3D == vertices[1])
-  p1other <- which(layout[[otherFace]]$points3D == vertices[1])
-  p2current <- which(layout[[currentFace]]$points3D == vertices[2])
-  p2other <- which(layout[[otherFace]]$points3D == vertices[2])
-  if (length(c(p1current,p1other,p2current,p2other)) != 4) stop("All vertices must occur in both faces")
-  
-  # rotate and translate the connected face
-  vecCurrent <- as.numeric(layout[[currentFace]]$coords2D[p2current,]) - as.numeric(layout[[currentFace]]$coords2D[p1current,])
-  vecOther <- as.numeric(layout[[otherFace]]$coords2D[p2other,]) - as.numeric(layout[[otherFace]]$coords2D[p1other,])
-  theta <-  vectorAngle2D(vecCurrent, vecOther)
-  rotation <- rotationMatrix2D(theta)
-  
-  # transform points P of other face
-  # as: p1current + rotation (P - p1other)
-  moved <- sweep(layout[[otherFace]]$coords2D, 2, as.numeric(layout[[otherFace]]$coords2D[p1other,]))
-  rotated <- t(rotation %*% t(moved))
-  translated <- sweep(rotated, 2, as.numeric(layout[[currentFace]]$coords2D[p1current,]), "+")
-  layout[[otherFace]]$coords2D <- translated
-  drawFace2D(layout[[otherFace]])
-  
-  cat("Placed:", otherFace, fill = T)
+  #
+  # now recurse, calling yourself with (newFace2D)
+  # 
 }
 
 evalLayout(layout)
